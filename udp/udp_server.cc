143d1849b1acd46f613d6bc6accd4b9551a275b3
TonyBeen <1772747354@qq.com>
2024-06-16 14:50:28 +0800

    update

--- udp/udp_server.cc
+++ udp/udp_server.cc
@@ -14,31 +14,31 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define BUFF_LEN 1024
-#define SERVER_IP "10.0.24.17"
-#define SERVER_PORT 9000
+#define BUFF_LEN        1400
+#define SERVER_PORT     9000
 
 void handle_udp_msg(int fd)
 {
-    char buf[BUFF_LEN];  //接收缓冲区，1024字节
+    char buf[BUFF_LEN];
     socklen_t len;
     int count;
-    struct sockaddr_in client_addr;  //clent_addr用于记录发送方的地址信息
+    struct sockaddr_in client_addr;
     while(1) {
         memset(buf, 0, BUFF_LEN);
         len = sizeof(client_addr);
-        count = recvfrom(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&client_addr, &len);  //recvfrom是拥塞函数，没有数据就一直拥塞
-        if(count == -1) {
-            printf("recieve data fail!\n");
-            return;
+        count = recvfrom(fd, buf, BUFF_LEN, 0, (struct sockaddr*)&client_addr, &len);
+        if (count == -1) {
+            perror("recvfrom error");
+            break;
         }
 
-        printf("[%s:%d]:%s,%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf, count);  //打印client发过来的信息
+        printf("[%s:%d]: %s, %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf, count);
 
         if (count > 0) {
-            int n = sprintf(buf, "I have recieved %d bytes data!\n", count);  // 回复client
-            sendto(fd, buf, n, 0, (struct sockaddr*)&client_addr, len);  //发送信息给client，注意使用了clent_addr结构体指针
+            int n = sprintf(buf, "I have recieved %d bytes data!\n", count);
+            sendto(fd, buf, n, 0, (struct sockaddr*)&client_addr, len);
         }
+        usleep(1000 * 1000);
     }
 }
 
@@ -48,7 +48,7 @@ int main(int argc, char* argv[])
     struct sockaddr_in addr;
     socklen_t len = sizeof(sockaddr_in);
 
-    server_fd = socket(AF_INET, SOCK_DGRAM, 0); // AF_INET:IPV4;SOCK_DGRAM:UDP
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
     if(server_fd < 0) {
         perror("create socket fail!");
         return -1;
@@ -56,8 +56,8 @@ int main(int argc, char* argv[])
 
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr(SERVER_IP); // IP地址，需要进行网络序转换，INADDR_ANY：本地地址
-    addr.sin_port = htons(SERVER_PORT);  // 端口号，需要网络序转换
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(SERVER_PORT);
 
     ret = bind(server_fd, (struct sockaddr*)&addr, len);
     if(ret < 0) {
