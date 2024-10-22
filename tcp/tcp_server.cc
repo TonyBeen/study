abbf2c2895668e77524bad1d3d1bef44247def46
TonyBeen <1772747354@qq.com>
2024-10-22 19:56:08 +0800

    update

--- tcp/tcp_server.cc
+++ tcp/tcp_server.cc
@@ -6,13 +6,15 @@
  ************************************************************************/
 
 #include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
+#include <string>
 #include <cstring>
 #include <thread>
-#include <arpa/inet.h>
+
 #include <getopt.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #define PORT 8080
 
@@ -32,21 +34,55 @@ void handle_client(int client_sock) {
 
 void start_server(int server_sock) {
     struct sockaddr_in address;
-    int addrlen = sizeof(address);
+    socklen_t addrlen = sizeof(address);
     
+    std::string peer_host;
+    uint16_t peer_port = 0;
     while (true) {
         // Accept incoming connection
-        int client_sock = accept(server_sock, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        int client_sock = accept(server_sock, (struct sockaddr*)&address, &addrlen);
         if (client_sock < 0) {
             std::cerr << "Failed to accept connection\n";
             continue;
         }
 
-        std::cout << "New connection from " << inet_ntoa(address.sin_addr) << ":" << ntohs(address.sin_port) << std::endl;
-
-        // Handle client in a new thread
-        std::thread(handle_client, client_sock).detach();
+        peer_host = inet_ntoa(address.sin_addr);
+        peer_port = ntohs(address.sin_port);
+        std::cout << "New connection from " << peer_host << ":" << peer_port << std::endl;
+        break;
     }
+
+    struct sockaddr_in bind_address;
+    // 尝试连接
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock < 0) {
+        printf("socket error, errno: %d, errstr: %s\n", errno, strerror(errno));
+        return;
+    }
+
+    int32_t opt = 1;
+    if (setsockopt(client_sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
+        perror("setsockopt(SO_REUSEPORT) error");
+        close(client_sock);
+        return;
+    }
+
+    bind_address.sin_family = AF_INET;
+    bind_address.sin_addr.s_addr = INADDR_ANY;
+    bind_address.sin_port = htons(PORT);
+    if (bind(client_sock, (struct sockaddr*)&bind_address, sizeof(bind_address)) < 0) {
+        perror("bind error");
+        close(client_sock);
+        return;
+    }
+
+    int ret = connect(client_sock, (struct sockaddr*)&address, sizeof(address));
+    if (ret < 0) {
+        printf("Failed to connect: [%d:%s]\n", errno, strerror(errno));
+        return;
+    }
+
+    printf("connect to %s:%u success\n", peer_host.c_str(), peer_port);
 }
 
 int32_t Server(bool is_client = false, const char *addr = nullptr)
@@ -55,14 +91,14 @@ int32_t Server(bool is_client = false, const char *addr = nullptr)
     struct sockaddr_in address;
     // Create socket file descriptor
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation failed\n";
+        perror("socket error");
         return -1;
     }
 
     // Enable SO_REUSEPORT
     int opt = 1;
     if (setsockopt(server_sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
-        std::cerr << "Failed to set SO_REUSEPORT\n";
+        perror("setsockopt(SO_REUSEPORT) error");
         close(server_sock);
         return -1;
     }
@@ -74,27 +110,45 @@ int32_t Server(bool is_client = false, const char *addr = nullptr)
 
     // Bind the socket to the network address and port
     if (bind(server_sock, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed\n";
+        perror("bind error");
         close(server_sock);
         return -1;
     }
 
     // Start listening for incoming connections
-    if (listen(server_sock, 10) < 0) {
-        std::cerr << "Listen failed\n";
+    if (listen(server_sock, 128) < 0) {
+        perror("listen error");
         close(server_sock);
         return -1;
     }
 
     if (is_client) {
+        int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (client_sock < 0) {
+            printf("socket error, errno: %d, errstr: %s\n", errno, strerror(errno));
+            return -1;
+        }
+
+        if (setsockopt(client_sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
+            perror("setsockopt(SO_REUSEPORT) error");
+            close(client_sock);
+            return -1;
+        }
+
+        if (bind(client_sock, (struct sockaddr*)&address, sizeof(address)) < 0) {
+            perror("bind error");
+            close(client_sock);
+            return -1;
+        }
+
         address.sin_addr.s_addr = inet_addr(addr);
-        int ret = connect(server_sock, (struct sockaddr*)&address, sizeof(address));
+        int ret = connect(client_sock, (struct sockaddr*)&address, sizeof(address));
         if (ret < 0) {
             printf("Failed to connect: [%d:%s]\n", errno, strerror(errno));
+            return 0;
         }
 
         printf("connect to %s success\n", addr);
-        return 0;
     }
 
     std::cout << "Server listening on port " << PORT << std::endl;
