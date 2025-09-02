ef862988671c2ba7e5b657db6845da690e214172
TonyBeen <1772747354@qq.com>
2025-09-02 16:31:47 +0800

    测试只用修复符号是否可以还原数据

--- raptorq/test_raptorq.cc
+++ raptorq/test_raptorq.cc
@@ -48,20 +48,19 @@ int main()
      * 一个文件经喷泉码编码生成多个block
      *
      * 一个blcok由多个symbol组成. block之间含有的symbol个数相差不超过1(测试结果)
-     * 
+     *
      * 一个symbol的大小由 symbol_size控制
      *
      * subsymbol 和 max_memory 以及 源数据大小 影响block中每个symbol个数
-     * 
+     *
      * 大概计算公式: 
      * block个数 = std::ceil(data_size / (max_memory * symbol_size / subsymbol))
-     * 
+     *
      * symbol个数 = std::ceil(data_size / symbol_size);
-     * 
+     *
      * block中含有symbol个数 = symbol个数 / block个数
-     * 
-     * symbol平均分到每个block中, 假设有17个symbol, 4个block, 不会是 [5, 5, 4, 3]这样分配
-     * 而是 [5, 4, 4, 4]
+     *
+     * symbol平均分到每个block中, 假设有17个symbol, 4个block, 不会是 [5, 5, 4, 3]这样分配而是 [5, 4, 4, 4]
      *
      * oti_scheme 和 oti_common 只与data_size subsymbol symbol_size max_memory有关, 与数据无关
      */
@@ -85,29 +84,31 @@ int main()
 
     auto blocks = RaptorQ_blocks(pEncoder);
     std::map<uint32_t, std::vector<uint8_t>> symbol_map;
+    uint16_t repair_symbol = 0;
+    uint16_t redundancy_symbol = 4;
     for (uint8_t block = 0; block < blocks; ++block) {
         uint32_t sym_for_blk = RaptorQ_symbols(pEncoder, block);
         printf("Block: %i, sym_for_blk = %u\n", block, sym_for_blk);
 
-        uint16_t repair_symbol = 1;
+        repair_symbol = sym_for_blk + redundancy_symbol; // 最少需要 sym_for_blk 个修复符号才能完整恢复数据
         uint32_t data_size = symbol_size / sizeof(uint8_t); // 因为类型选的是 ENC_8
 
         // 编码符号
-        for (uint32_t source = 0; source < sym_for_blk; ++source) {
-            uint32_t id = RaptorQ_id(source, block);
-            printf("symbol Id: %u\n", id);
-            auto &symbol_vec = symbol_map[id];
-            symbol_vec.reserve(data_size);
+        // for (uint32_t source = 0; source < sym_for_blk; ++source) {
+        //     uint32_t id = RaptorQ_id(source, block);
+        //     printf("symbol Id: %u\n", id);
+        //     auto &symbol_vec = symbol_map[id];
+        //     symbol_vec.reserve(data_size);
 
-            uint8_t *data_buffer = symbol_vec.data();
-            uint64_t written = RaptorQ_encode(pEncoder, (void **)&data_buffer, data_size, source, block);
-            if (written != data_size) {
-                fprintf(stderr, "Error in getting source symbol\n");
-                RaptorQ_free(&pEncoder);
-                return -1;
-            }
-            // data_buffer 现在指向 data_buffer + written位置
-        }
+        //     uint8_t *data_buffer = symbol_vec.data();
+        //     uint64_t written = RaptorQ_encode(pEncoder, (void **)&data_buffer, data_size, source, block);
+        //     if (written != data_size) {
+        //         fprintf(stderr, "Error in getting source symbol\n");
+        //         RaptorQ_free(&pEncoder);
+        //         return -1;
+        //     }
+        //     // data_buffer 现在指向 data_buffer + written位置
+        // }
 
         // 获取修复符号
         uint32_t max_repair_symbol = RaptorQ_max_repair (pEncoder, block);
@@ -126,6 +127,8 @@ int main()
                 return -1;
             }
         }
+
+        repair_symbol = sym_for_blk + redundancy_symbol;
     }
 
     // 编码完毕 释放资源 获取解码所需数据
@@ -143,7 +146,9 @@ int main()
     blocks = RaptorQ_blocks(pDecode);
     printf("blocks = %u\n", blocks);
 
-    for (auto it = symbol_map.begin(); it != symbol_map.end(); ++it) {
+    auto it = symbol_map.begin();
+    ++it; // 测试跳过第一个数据能否正常解码
+    for (uint16_t i = 0; i < (repair_symbol - redundancy_symbol); ++i, ++it) {
         // printf("Id = %u\n", it->first);
         uint8_t *symbol_data = it->second.data();
         uint32_t symbol_data_size = RaptorQ_symbol_size(pDecode) / sizeof(uint8_t);
